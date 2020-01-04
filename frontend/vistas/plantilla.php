250b20be8dd0df0f97cf8ad3f6bbb00c57ecf2c3
MIGUEL <miguelantiman.mav@gmail.com>
2020-01-04 19:44:24 -0300

    maquetado y estilo del header

---
+++ frontend/vistas/plantilla.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="title" content="Tienda Virtual">
+    <meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi earum placeat quisquam fugiat.">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tienda Virtual - AntvasWeb</title>
+
+    <link rel="stylesheet" href="vistas/css/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="vistas/css/plugins/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu+Condensed|Ubuntu:400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="vistas/css/plantilla.css">
+    <link rel="stylesheet" href="vistas/css/cabezote.css">
+    <script src="vistas/js/plugins/jquery.min.js"></script>
+    <script src="vistas/js/plugins/bootstrap.min.js"></script>
+</head>
+
+<body>
+<?php    
+/*=============================================
+CABEZOTE
+=============================================*/
+include 'modulos/cabezote.php';
+?>
+
+</body>
+
+</html>
